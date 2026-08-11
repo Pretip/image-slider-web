e70afeef908e7ffac340824b5332f7d17d14ac28
Pretip <kaviarasupretip@yahoo.com>
2026-08-11 17:59:05 +0800

    updated main test file

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -18,9 +18,15 @@ test('Launch web and fill in credentials', async ({ page }) => {
   //Verify the login was successful and redirects to main page
   await expect(page).toHaveTitle('Image Slider - Main');
 
+  //Scroll down bottom of the page
+  await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+
   // Upload photo and caption
-  //await page.locator('#caption-input').fill('Its a beautiful beach. This is automation');
-  //await page.getByRole('button', { name: 'Upload Photo'}).click();
+  await page.locator('#caption-input').fill('This is playwright automation');
+
+  await page.locator('input[type="file"]').setInputFiles('C:/Users/ASUS/Javascript Projects/image-slider-web/photos/test/bali.jpg');
+
+  await page.getByText('Upload Photo').click();
 
 });
 
